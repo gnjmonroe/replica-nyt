36136c6812e92e3fcc0665de13f5bbfe894ba344
gnjmonroe <gnjmonroe@gmail.com>
2021-04-19 05:59:46 +0900

    update for desktop layouts

--- script.js
+++ script.js
@@ -1,107 +1,107 @@
-"use strict";
-// grab HTML elements for site-index accordion toggling
-var headerSiteIndex0 = document.getElementById("header-siteIndex0");
-var bodySiteIndex0 = document.getElementById("body-siteIndex0");
-var headerSiteIndex1 = document.getElementById("header-siteIndex1");
-var bodySiteIndex1 = document.getElementById("body-siteIndex1");
-var headerSiteIndex2 = document.getElementById("header-siteIndex2");
-var bodySiteIndex2 = document.getElementById("body-siteIndex2");
-var headerSiteIndex3 = document.getElementById("header-siteIndex3");
-var bodySiteIndex3 = document.getElementById("body-siteIndex3");
-var headerSiteIndex4 = document.getElementById("header-siteIndex4");
-var bodySiteIndex4 = document.getElementById("body-siteIndex4");
-headerSiteIndex0.onclick = testLog;
-headerSiteIndex1.onclick = testLog;
-headerSiteIndex2.onclick = testLog;
-headerSiteIndex3.onclick = testLog;
-headerSiteIndex4.onclick = testLog;
+const headerAccordion0 = document.getElementById('header-accordion-0');
+const bodyAccordion0 = document.getElementById('body-accordion-0');
+const headerAccordion1 = document.getElementById('header-accordion-1');
+const bodyAccordion1 = document.getElementById('body-accordion-1');
+const headerAccordion2 = document.getElementById('header-accordion-2');
+const bodyAccordion2 = document.getElementById('body-accordion-2');
+const headerAccordion3 = document.getElementById('header-accordion-3');
+const bodyAccordion3 = document.getElementById('body-accordion-3');
+const headerAccordion4 = document.getElementById('header-accordion-4');
+const bodyAccordion4 = document.getElementById('body-accordion-4');
+
+headerAccordion0.onclick = testLog;
+headerAccordion1.onclick = testLog;
+headerAccordion2.onclick = testLog;
+headerAccordion3.onclick = testLog;
+headerAccordion4.onclick = testLog;
+
 function testLog() {
-    // close all other headers
-    switch (this.id.charAt(this.id.length - 1)) {
-        case '0':
-            headerSiteIndex1.classList.remove("active");
-            headerSiteIndex2.classList.remove("active");
-            headerSiteIndex3.classList.remove("active");
-            headerSiteIndex4.classList.remove("active");
-            bodySiteIndex1.classList.remove("active");
-            bodySiteIndex2.classList.remove("active");
-            bodySiteIndex3.classList.remove("active");
-            bodySiteIndex4.classList.remove("active");
-            break;
-        case '1':
-            headerSiteIndex0.classList.remove("active");
-            headerSiteIndex2.classList.remove("active");
-            headerSiteIndex3.classList.remove("active");
-            headerSiteIndex4.classList.remove("active");
-            bodySiteIndex0.classList.remove("active");
-            bodySiteIndex2.classList.remove("active");
-            bodySiteIndex3.classList.remove("active");
-            bodySiteIndex4.classList.remove("active");
-            break;
-        case '2':
-            headerSiteIndex0.classList.remove("active");
-            headerSiteIndex1.classList.remove("active");
-            headerSiteIndex3.classList.remove("active");
-            headerSiteIndex4.classList.remove("active");
-            bodySiteIndex0.classList.remove("active");
-            bodySiteIndex1.classList.remove("active");
-            bodySiteIndex3.classList.remove("active");
-            bodySiteIndex4.classList.remove("active");
-            break;
-        case '3':
-            headerSiteIndex0.classList.remove("active");
-            headerSiteIndex1.classList.remove("active");
-            headerSiteIndex2.classList.remove("active");
-            headerSiteIndex4.classList.remove("active");
-            bodySiteIndex0.classList.remove("active");
-            bodySiteIndex1.classList.remove("active");
-            bodySiteIndex2.classList.remove("active");
-            bodySiteIndex4.classList.remove("active");
-            break;
-        case '4':
-            headerSiteIndex0.classList.remove("active");
-            headerSiteIndex1.classList.remove("active");
-            headerSiteIndex2.classList.remove("active");
-            headerSiteIndex3.classList.remove("active");
-            bodySiteIndex1.classList.remove("active");
-            bodySiteIndex2.classList.remove("active");
-            bodySiteIndex3.classList.remove("active");
-            bodySiteIndex0.classList.remove("active");
-            break;
-    }
-    // toggle this item's active class
-    this.classList.toggle("active");
-    this.nextElementSibling.classList.toggle("active");
+  // close all other headers
+  switch (this.id.charAt(this.id.length - 1)) {
+      case '0':
+          headerAccordion1.classList.remove("active");
+          headerAccordion2.classList.remove("active");
+          headerAccordion3.classList.remove("active");
+          headerAccordion4.classList.remove("active");
+          bodyAccordion1.classList.remove("active");
+          bodyAccordion2.classList.remove("active");
+          bodyAccordion3.classList.remove("active");
+          bodyAccordion4.classList.remove("active");
+          break;
+      case '1':
+          headerAccordion0.classList.remove("active");
+          headerAccordion2.classList.remove("active");
+          headerAccordion3.classList.remove("active");
+          headerAccordion4.classList.remove("active");
+          bodyAccordion0.classList.remove("active");
+          bodyAccordion2.classList.remove("active");
+          bodyAccordion3.classList.remove("active");
+          bodyAccordion4.classList.remove("active");
+          break;
+      case '2':
+          headerAccordion0.classList.remove("active");
+          headerAccordion1.classList.remove("active");
+          headerAccordion3.classList.remove("active");
+          headerAccordion4.classList.remove("active");
+          bodyAccordion0.classList.remove("active");
+          bodyAccordion1.classList.remove("active");
+          bodyAccordion3.classList.remove("active");
+          bodyAccordion4.classList.remove("active");
+          break;
+      case '3':
+          headerAccordion0.classList.remove("active");
+          headerAccordion1.classList.remove("active");
+          headerAccordion2.classList.remove("active");
+          headerAccordion4.classList.remove("active");
+          bodyAccordion0.classList.remove("active");
+          bodyAccordion1.classList.remove("active");
+          bodyAccordion2.classList.remove("active");
+          bodyAccordion4.classList.remove("active");
+          break;
+      case '4':
+          headerAccordion0.classList.remove("active");
+          headerAccordion1.classList.remove("active");
+          headerAccordion2.classList.remove("active");
+          headerAccordion3.classList.remove("active");
+          bodyAccordion1.classList.remove("active");
+          bodyAccordion2.classList.remove("active");
+          bodyAccordion3.classList.remove("active");
+          bodyAccordion0.classList.remove("active");
+          break;
+  }
+  // toggle this item's active class
+  this.childNodes[3].classList.toggle("active");
+  // this.nextElementSibling.classList.toggle("active");
 }
-var slideIndexTGR = -1;
-function theGreatReadSlideshow() {
-    var i;
-    var slidesTGR = document.getElementsByClassName("fader-inner");
-    for (i = 1; i < slidesTGR[0].childNodes.length; i += 2) {
-        slidesTGR[0].childNodes[i].classList.remove("active");
-    }
-    slideIndexTGR += 2;
-    if (slideIndexTGR >= slidesTGR[0].childNodes.length) {
-        slideIndexTGR = 1;
-    }
-    slidesTGR[0].childNodes[(slideIndexTGR - 2 == -1) ? 7 : slideIndexTGR - 2].classList.add("active");
-    setTimeout(theGreatReadSlideshow, 7000);
+var stockIndex = 0;
+function stockSlideshow() {
+  var i;
+  var slideStocks = document.getElementsByClassName('stock-tab');
+  for (i = 0; i < slideStocks.length; i++) {
+    slideStocks[i].classList.add('hide-stock');
+  }
+  stockIndex++;
+  if (stockIndex >= slideStocks.length) {
+    stockIndex = 0;
+  }
+  slideStocks[stockIndex].classList.remove('hide-stock');
+  setTimeout(stockSlideshow, 3000);
 }
-theGreatReadSlideshow();
+stockSlideshow();
+
 function openModal() {
-    var modal = document.getElementsByClassName("mobile-portal");
-    modal[0].style.display = 'block';
+  var modal = document.getElementsByClassName("mobile-portal");
+  modal[0].style.display = 'block';
 }
 function closeModal() {
-    var modal = document.getElementsByClassName("mobile-portal");
-    modal[0].style.display = 'none';
+  var modal = document.getElementsByClassName("mobile-portal");
+  modal[0].style.display = 'none';
 }
 function openAccountModal() {
-    var modal = document.getElementsByClassName("mobile-account-portal");
-    modal[0].style.display = 'block';
+  var modal = document.getElementsByClassName("mobile-account-portal");
+  modal[0].style.display = 'block';
 }
 function closeAccountModal() {
-    var modal = document.getElementsByClassName("mobile-account-portal");
-    modal[0].style.display = 'none';
+  var modal = document.getElementsByClassName("mobile-account-portal");
+  modal[0].style.display = 'none';
 }
-//# sourceMappingURL=script.js.map
